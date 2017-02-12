b9a5778653baa7928d802ccf3053121ba053efa1
eric <baby3873575@gmail.com>
2017-02-12 23:33:46 +0800

    kk

--- complete/src/main/java/hello/ScheduledTasks.java
+++ complete/src/main/java/hello/ScheduledTasks.java
@@ -8,6 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+	
 @Component
 public class ScheduledTasks {
 
@@ -15,8 +26,19 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+	
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+		log.info("The time is now {}", dateFormat.format(new Date()));
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("https://beta-ssp.tenmax.io/supply/mobile/native/rmax-ad?rmaxSpaceId=55ba76bca772421f&dpid=bd4b9b7903cf40ce&v=1", String.class);
+
+		//log.info(response.getBody().toString());
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = mapper.readTree(response.getBody());
+		JsonNode name = root.path("title");
+		log.info(name.asText());
+
     }
 }
